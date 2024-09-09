1bf19e5f5a95d5f2c4850e7135e284952a3e237f
Eduard-Condurache <eduardcondurache96@gmail.com>
2024-09-09 16:57:28 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  data() {
+    return {
+      count: 0
+
+    }
+  },
+  components: {
+    AppHeader
+  },
+  methods: {
+    incrementCount() {
+      this.count++;
+    }
+  } 
+}
+</script>
+
+<template>
+  <div>
+    <AppHeader/>
+    <main>
+      <button @click="incrementCount()">
+        {{ count }}
+      </button>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+
+main {
+  text-align: center;
+}
+
+main button {
+  padding: 14px;
+  background-color: lightcoral;
+  border: none;
+  border-radius: 14px;
+}
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'Template Vite + Vue'
+
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      {{ message }}
+    </h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: lightgreen;
+  text-align: center;
+  margin-bottom: 14px;
+  padding: 20px;
+}
+
+</style>
